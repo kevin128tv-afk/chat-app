d6bec60ebbec08ffcbe035e15565139d354d8928
kevin128tv-afk <kevin128tv@gmail.com>
2026-07-10 22:58:20 +0900

    Add files via upload

---
+++ server/Message.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    messageType: {
+        type: String,
+        enum: ['text', 'image'],
+        default: 'text'
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
